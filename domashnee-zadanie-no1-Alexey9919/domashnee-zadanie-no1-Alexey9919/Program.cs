6666b161984581f71838c3fc08559ca8068256b5
Alexey9919 <90642794+Alexey9919@users.noreply.github.com>
2021-09-18 15:29:47 +0400

    Выполнено

---
+++ domashnee-zadanie-no1-Alexey9919/domashnee-zadanie-no1-Alexey9919/Program.cs
@@ -0,0 +1,22 @@
+﻿//Даны переменные A, B, C. Изменить их значения, переместив содержимое A в C, C — в B, B — в A, и вывести новые значения переменных A,B, C.
+using System;
+
+namespace DZ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Enter the variable a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the variable b");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the variable c");
+            c = Convert.ToDouble(Console.ReadLine());
+            (a, b, c) = (b, c, a);
+            Console.WriteLine((a, b, c));
+
+        }
+    }
+}
